ea70de108fd194e68fb0c1d0a66f491133653cd0
juhanir <juhani.riisio@gmail.com>
2020-02-08 20:47:48 +0200

    add queries

--- queries.py
+++ queries.py
@@ -1,29 +1,40 @@
-INIT_DB = """ DROP TABLE IF EXISTS Asiakas;
-              DROP TABLE IF EXISTS Paikka;
-              DROP TABLE IF EXISTS Paketti;
-              DROP TABLE IF EXISTS Tapahtuma;
-              CREATE TABLE Asiakas (
-                id INTEGER PRIMARY KEY,
-                nimi TEXT NOT NULL UNIQUE
-              );
-              CREATE TABLE Paikka (
-                id INTEGER PRIMARY KEY,
-                nimi TEXT NOT NULL UNIQUE
-              );
-              CREATE TABLE Paketti (
-                koodi VARCHAR(200) PRIMARY KEY,
-                asiakas_id INTEGER,
-                FOREIGN KEY(asiakas_id) REFERENCES Asiakas(id)
-              );
-              CREATE TABLE Tapahtuma (
-                id INTEGER PRIMARY KEY,
-                kuvaus TEXT,
-                ajankohta TEXT NOT NULL,
-                paketti_koodi VARCHAR(200),
-                paikka_id INTEGER,
-                FOREIGN KEY(paketti_koodi) REFERENCES Paketti(id),
-                FOREIGN KEY(paikka_id) REFERENCES Paikka(id)
-              ); """
+INIT_DB =           """ DROP TABLE IF EXISTS Asiakas;
+                        DROP TABLE IF EXISTS Paikka;
+                        DROP TABLE IF EXISTS Paketti;
+                        DROP TABLE IF EXISTS Tapahtuma;
+                        PRAGMA foreign_keys = ON;
+                        CREATE TABLE Asiakas (
+                          nimi TEXT PRIMARY KEY
+                        );
+                        CREATE TABLE Paikka (
+                          nimi TEXT PRIMARY KEY
+                        );
+                        CREATE TABLE Paketti (
+                          koodi VARCHAR(200) PRIMARY KEY,
+                          asiakas_nimi TEXT,
+                          FOREIGN KEY(asiakas_nimi) REFERENCES Asiakas(nimi)
+                        );
+                        CREATE TABLE Tapahtuma (
+                          id INTEGER PRIMARY KEY,
+                          paketti_koodi VARCHAR(200),
+                          ajankohta TEXT NOT NULL,
+                          paikka_nimi TEXT,
+                          kuvaus TEXT,
+                          FOREIGN KEY(paketti_koodi) REFERENCES Paketti(koodi),
+                          FOREIGN KEY(paikka_nimi) REFERENCES Paikka(nimi)
+                        ); """
+
+ADD_LOCATION =      """INSERT INTO Paikka(nimi) VALUES (?);"""
+ADD_CUSTOMER =      """INSERT INTO Asiakas(nimi) VALUES (?);"""
+ADD_PARCEL =        """INSERT INTO Paketti(koodi, asiakas_nimi) VALUES (?,?);"""
+ADD_EVENT =         """INSERT INTO Tapahtuma(paketti_koodi, paikka_nimi, kuvaus, ajankohta) VALUES (?,?,?,datetime('now', 'localtime'));"""
+GET_EVENTS_FOR_PARCEL = """SELECT ajankohta, paikka_nimi, kuvaus FROM Tapahtuma WHERE paketti_koodi=?;"""
+GET_PARCELS_FOR_CUSTOMER = """SELECT koodi, (SELECT count(*) FROM Tapahtuma WHERE paketti_koodi = P.koodi) FROM Paketti P WHERE asiakas_nimi LIKE ?;"""
+
+PAIKAT =            """SELECT * FROM Paikka;"""
+ASIAKKAAT =         """SELECT * FROM Asiakas;"""
+PAKETIT =           """SELECT * FROM Paketti;"""
+TAPAHTUMAT =        """SELECT * FROM Tapahtuma;"""
 
 SET_TEST_VALUES = """ INSERT INTO Asiakas(nimi) VALUES ('Kiipeli'), ('Juukeli'), ('Peepeli'), ('Aapeli');
                       INSERT INTO Paikka(nimi) VALUES ('Kouvola'), ('Kotka'), ('Hamina'), ('Pyhtää');
--- utils.py
+++ utils.py
@@ -1,12 +1,75 @@
 import re
+import queries
 
-def cast_input(input_str: str) -> bool:
-  """Check that input value is allowed, otherwise complain with raised exception."""
-  input_str = str.strip(input_str)
-  if re.match('^[1-9]{1}$', input_str) is None:
-    raise Exception('Input values must be between 1-9')
-  else:
-    return int(input_str)
+def cast_input(input_str: str) -> int:
+    """Check that input value is allowed, otherwise complain with raised exception."""
+    input_str = str.strip(input_str)
+    if re.match('^[0-9]{1}$', input_str) is None:
+        raise Exception('Input values must be between 0-9')
+    else:
+        return int(input_str)
 
-if __name__ == "__main__":
-    cast_input()
+def no_op(cursor):
+    cursor.execute(queries.PAIKAT)
+    print('{}'.format('PAIKAT:'))
+    print('{}'.format(cursor.fetchall()))
+    cursor.execute(queries.ASIAKKAAT)
+    print('{}'.format('ASIAKKAAT:'))
+    print('{}'.format(cursor.fetchall()))
+    cursor.execute(queries.TAPAHTUMAT)
+    print('{}'.format('TAPAHTUMAT:'))
+    print('{}'.format(cursor.fetchall()))
+    cursor.execute(queries.PAKETIT)
+    print('{}'.format('PAKETIT:'))
+    print('{}'.format(cursor.fetchall()))
+
+
+def add_tables(cursor):
+    cursor.executescript(queries.INIT_DB)
+
+def add_location(cursor, location):
+    cursor.execute(queries.ADD_LOCATION, location)
+    print('Paikka lisätty id:llä {}'.format(cursor.lastrowid))
+
+def add_customer(cursor, name):
+    cursor.execute(queries.ADD_CUSTOMER, name)
+    print('Asiakas lisätty id:llä {}'.format(cursor.lastrowid))
+
+def add_parcel(cursor, params):
+    cursor.execute(queries.ADD_PARCEL, params)
+    print('Paketti lisätty id:llä {}'.format(cursor.lastrowid))
+
+def add_event(cursor, params):
+    cursor.execute(queries.ADD_EVENT, params)
+    print('Tapahtuma lisätty id:llä {}'.format(cursor.lastrowid))
+
+def get_events_for_parcel(cursor, code):
+    cursor.execute(queries.GET_EVENTS_FOR_PARCEL, code)
+    print('{}'.format(cursor.fetchall()))
+
+def get_parcels_for_customer(cursor, name):
+    cursor.execute(queries.GET_PARCELS_FOR_CUSTOMER, name)
+    print('{}'.format(cursor.fetchall()))
+
+def execute_action(action_num: int, cursor):
+    """Map input number to correct action and execute it with args when applicable"""
+    action_mapper = {
+        0: [no_op],
+        1: [add_tables],
+        2: [add_location, 'Anna paikan nimi:'],
+        3: [add_customer, 'Anna asiakkaan nimi:'],
+        4: [add_parcel, 'Anna paketin koodi:', 'Anna paketin asiakas:'],
+        5: [add_event, 'Anna paketin koodi:', 'Anna tapahtuman paikka:', 'Anna tapahtuman kuvaus:'],
+        6: [get_events_for_parcel, 'Anna paketin koodi:'],
+        7: [get_parcels_for_customer, 'Anna asiakkaan nimi:']
+    }
+    action = action_mapper.get(action_num, 0)
+    if len(action) > 1:
+        params = []
+        for prompt in action[1:]:
+            print('{}'.format(prompt))
+            user_input = input()
+            params.append(user_input)
+        action[0](cursor, params)
+    else:
+        action[0](cursor)
